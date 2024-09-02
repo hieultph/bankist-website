5619147987cf7d8d6c3c44d80ab2e6dcae41a4fa
Andrew Le <hieultph@gmail.com>
2024-09-02 11:54:55 +0700

    Tabbed updated

--- script.js
+++ script.js
@@ -66,3 +66,32 @@ document.querySelector('.nav__links').addEventListener('click', e => {
       .scrollIntoView({ behavior: 'smooth' });
   }
 });
+
+/////////////////////////////////////
+// Tabbed component
+// Click on tab -> active elements and display content + deactivate other elements
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+  console.log(clicked);
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  console.log('hello!');
+
+  // Activate tab
+  clicked.classList.add('operations__tab--active');
+
+  // Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
+});
